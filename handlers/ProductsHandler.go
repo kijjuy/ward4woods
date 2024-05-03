2ba9a025f25934d49816477f3dcc3dd760c88144
kijjuy <brandonzomer@proton.me>
2024-05-03 17:45:39 -0400

    move handlers to package

---
+++ handlers/ProductsHandler.go
@@ -0,0 +1,111 @@
+package handlers
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"strconv"
+	"strings"
+
+	"ward4woods.ca/data"
+	"ward4woods.ca/models"
+)
+
+type ProductsHandler struct {
+	productsStore *data.ProductsStore
+	logger        *slog.Logger
+}
+
+func NewProductsHandler(productsStore *data.ProductsStore, logger *slog.Logger) *ProductsHandler {
+	return &ProductsHandler{
+		productsStore: productsStore,
+		logger:        logger.With("Location", "ProductsHandler"),
+	}
+}
+
+func (ph *ProductsHandler) GetAllProducts(w http.ResponseWriter, r *http.Request) {
+	products, err := ph.productsStore.GetAllProducts()
+
+	if err != nil {
+		ph.logger.Error("Could not get products from store. Writing error response.")
+		fmt.Fprintf(w, "Server error when trying to load products.")
+		return
+	}
+
+	json.NewEncoder(w).Encode(products)
+}
+
+func (ph *ProductsHandler) GetProductById(w http.ResponseWriter, r *http.Request) {
+	id, err := ph.getIdFromRequest(w, r)
+	if err != nil {
+		return
+	}
+
+	product, err := ph.productsStore.GetProductById(id)
+
+	if err == sql.ErrNoRows {
+		fmt.Fprint(w, "Product not found.")
+		ph.logger.Info("Attempted to find product by id, but product didn't exist.")
+		return
+	}
+
+	if err != nil {
+		fmt.Fprint(w, "Error finding product.")
+		ph.logger.Warn("Error when finding product from database.")
+		return
+	}
+
+	json.NewEncoder(w).Encode(product)
+}
+
+func (ph *ProductsHandler) CreateProduct(w http.ResponseWriter, r *http.Request) {
+	product := models.Product{}
+	err := json.NewDecoder(r.Body).Decode(&product)
+
+	if err != nil {
+		ph.logger.Error("Could not parse product from body. Error:", err)
+		fmt.Fprintf(w, "Error parsing product from json.")
+		return
+	}
+
+	err = ph.productsStore.AddProduct(product)
+
+	if err != nil {
+		ph.logger.Warn("Error adding product to product store.")
+		fmt.Fprintf(w, "Error adding product to database.")
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
+func (ph *ProductsHandler) DeleteProductById(w http.ResponseWriter, r *http.Request) {
+	id, err := ph.getIdFromRequest(w, r)
+
+	if err != nil {
+		return
+	}
+
+	err = ph.productsStore.DeleteProductById(id)
+
+	if err != nil {
+		fmt.Fprintf(w, "Error deleting product.")
+		ph.logger.Warn("Could not delete product from products store.")
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
+func (ph *ProductsHandler) getIdFromRequest(w http.ResponseWriter, r *http.Request) (int, error) {
+	idStr := strings.TrimPrefix(r.URL.String(), "/api/products/")
+	id, err := strconv.Atoi(idStr)
+
+	if err != nil {
+		ph.logger.Warn("Could not get products by id: invalid id. Error: ", err)
+		fmt.Fprintf(w, "Invalid product id.")
+	}
+
+	return id, err
+}
--- main.go
+++ main.go
@@ -2,17 +2,15 @@ package main
 
 import (
 	"database/sql"
-	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
-	"strconv"
-	"strings"
 
 	"log/slog"
 
 	_ "github.com/lib/pq"
 	"ward4woods.ca/data"
+	"ward4woods.ca/handlers"
 )
 
 func createDb(conString string, logger *slog.Logger) *sql.DB {
@@ -30,45 +28,6 @@ func createProductsStore(db *sql.DB, logger *slog.Logger) *data.ProductsStore {
 	return data.NewProductsStore(db, logger)
 }
 
-type ProductsHandler struct {
-	productsStore *data.ProductsStore
-	logger        *slog.Logger
-}
-
-func NewProductsHandler(productsStore *data.ProductsStore, logger *slog.Logger) *ProductsHandler {
-	return &ProductsHandler{productsStore: productsStore, logger: logger}
-}
-
-func (ph *ProductsHandler) GetAllProducts(w http.ResponseWriter, r *http.Request) {
-	products, err := ph.productsStore.GetAllProducts()
-
-	if err != nil {
-		ph.logger.Error("Could not get products from store. Writing error response.")
-		fmt.Fprintf(w, "Server error when trying to load products.")
-		return
-	}
-
-	json.NewEncoder(w).Encode(products)
-}
-
-func (ph *ProductsHandler) GetProductById(w http.ResponseWriter, r *http.Request) {
-	ph.logger.Info("Url: ", r.URL)
-	idStr := strings.TrimPrefix(r.URL.String(), "/api/products/")
-	id, err := strconv.Atoi(idStr)
-	if err != nil {
-		ph.logger.Warn("Could not get products by id: invalid id. Error: ", err)
-		fmt.Fprintf(w, "Invalid product id.")
-	}
-
-	product, err := ph.productsStore.GetProductById(id)
-
-	if err != nil {
-		ph.logger.Error("Could not get product from database.")
-	}
-
-	json.NewEncoder(w).Encode(product)
-}
-
 func main() {
 	port := ":8080"
 	logger := slog.Default()
@@ -78,13 +37,23 @@ func main() {
 
 	productsStore := createProductsStore(db, logger)
 
-	productsHandler := NewProductsHandler(productsStore, logger)
+	productsHandler := handlers.NewProductsHandler(productsStore, logger)
 
-	http.HandleFunc("/api/products", productsHandler.GetAllProducts)
-	http.HandleFunc("/api/products/{id}", productsHandler.GetProductById)
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/api/products", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodGet:
+			productsHandler.GetAllProducts(w, r)
+		case http.MethodPost:
+			productsHandler.CreateProduct(w, r)
+		}
+	})
+
+	mux.HandleFunc("/api/products/{id}", productsHandler.GetProductById)
 
 	logger.Info(fmt.Sprintf("Application now lisening at: localhost%s", port))
-	err := http.ListenAndServe(port, nil)
+	err := http.ListenAndServe(port, mux)
 
 	logger.Error("Application crashed. Error:", err)
 }
