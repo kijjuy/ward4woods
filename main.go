fbc92daa8d6c6effa2c330f2e399df8bac77a4ec
kijjuy <brandonzomer@proton.me>
2024-05-03 16:21:23 -0400

    add data store creation

--- main.go
+++ main.go
@@ -1,9 +1,39 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"os"
+
+	"log/slog"
+
+	_ "github.com/lib/pq"
+	"ward4woods.ca/data"
 )
 
+func createDb(conString string, logger *slog.Logger) *sql.DB {
+	logger.Info("Connecting to database...")
+	db, err := sql.Open("postgres", conString)
+	if err != nil {
+		logger.Error("Could not connect to database. Error:", err)
+		os.Exit(1)
+	}
+	logger.Info("Database connection established.")
+	return db
+}
+
+func createProductsStore(db *sql.DB, logger *slog.Logger) *data.ProductsStore {
+	return data.NewProductsStore(db, logger)
+}
+
 func main() {
-	fmt.Println("Hello world")
+	logger := slog.Default()
+
+	conString := "postgres://postgres@172.17.0.2:5432?password=Password@1&sslmode=disable"
+	db := createDb(conString, logger)
+
+	productsStore := createProductsStore(db, logger)
+
+	productsHandler := NewProductsHandler(productsStore, logger)
+
 }
