d1977bb9d379558d20f6cab5cc69590459da0b8c
kijjuy <brandonzomer@proton.me>
2024-05-07 15:34:31 -0400

    create helper to render templates

---
+++ helpers/templateHelper.go
@@ -0,0 +1,23 @@
+package helpers
+
+import (
+	"html/template"
+	"log/slog"
+	"net/http"
+)
+
+func RenderTemplate(w http.ResponseWriter, templateName string, data interface{}, logger *slog.Logger) {
+	logger = logger.With("Location", "RenderTemplate")
+	tmpl, err := template.ParseFiles(templateName)
+	if err != nil {
+		logger.Error("Could not load template files.", "Error", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	err = tmpl.Execute(w, data)
+	if err != nil {
+		logger.Error("Could not execute template.", "Error", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+}
