51d902402b218d28bada4ebe87d2e9fa77e35082
kijjuy <brandonzomer@proton.me>
2024-05-28 15:23:21 -0400

    remove ProductDetails from productHandler

--- handlers/ProductsHandlerTemplate.go
+++ handlers/ProductsHandlerTemplate.go
@@ -24,32 +24,3 @@ func (ph *ProductsHandler) getIdFromTemplateRequest(w http.ResponseWriter, r *ht
 	prefix := "/products/"
 	return helpers.GetIdFromRequest(w, r, prefix)
 }
-
-// ProductsDetails handles creating the individual page for each product.
-// Will be sent requests from the URL '/products/{id}'
-func (ph *ProductsHandler) ProductsDetails(w http.ResponseWriter, r *http.Request, productsStore *data.ProductsStore) {
-	id, err := helpers.GetIdFromRequest(w, r, "/products/")
-	if err != nil {
-		return
-	}
-
-	product, err := productsStore.GetProductById(id)
-
-	if err == sql.ErrNoRows {
-		ph.logger.Warn("Attempted to find product by id, but product didn't exist.")
-		http.Error(w, "Product not found.", http.StatusNotFound)
-		return
-	}
-
-	if err != nil {
-		ph.logger.Warn("Error when finding product from database.")
-		http.Error(w, "Error finding product.", http.StatusInternalServerError)
-		return
-	}
-
-	ph.logger.Info(fmt.Sprintf("now serving details page for product: %+v", product))
-
-	helpers.RenderTemplate(w, "templates/productDetails.html", product)
-
-	//TODO: Setup details page to work with htmx rather than loading 2 templates form go
-}
