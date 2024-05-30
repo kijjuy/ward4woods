5df97f3bdb8e9a13441c23e2ab415f3c26759487
kijjuy <brandonzomer@proton.me>
2024-05-30 11:20:11 -0400

    change flake to the-nix-way#go

--- flake.nix
+++ flake.nix
@@ -1,9 +1,31 @@
 {
-  description = "A very basic flake";
+  description = "A Nix-flake-based Go 1.22 development environment";
 
-  inputs = {
-      go-dev.url = "github:kijjuy/nix-flakes?dir=go";
-  };
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
 
-  outputs = { self, go-dev }: go-dev.outputs;
+  outputs = { self, nixpkgs }:
+    let
+      goVersion = 22; # Change this to update the whole stack
+      overlays = [ (final: prev: { go = prev."go_1_${toString goVersion}"; }) ];
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit overlays system; };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            # go 1.22 (specified by overlay)
+            go_1_22
+
+            # goimports, godoc, etc.
+            gotools
+
+            # https://github.com/golangci/golangci-lint
+            golangci-lint
+          ];
+        };
+      });
+    };
 }
